a814d869af5eafefaebb5065ab126330dacf5db0
gmy <gaomengyao@microconnect.com>
2023-10-24 13:48:19 +0800

    提交格式化代码

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>ffffffffffffffffff</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+for (let index = 0; index < 100; index++) 
+{
+  console.log(index);
+  
+}
+</script>
+
+<style lang="scss" scoped></style>
